28283645f7e636286eb6eefeef782de4b349e90d
Marcus Müller <marcus.mueller@ettus.com>
2016-06-30 18:01:07 +0200

    cleaned up noc_block_zero_pad.v

--- usrp3/lib/rfnoc/noc_block_zero_pad.v
+++ usrp3/lib/rfnoc/noc_block_zero_pad.v
@@ -1,3 +1,7 @@
+//
+// Copyright 2016 Ettus Research
+//
+
 module noc_block_zero_pad #(
   parameter NOC_ID = 64'h6667_0000_0000_0000,
   parameter STR_SINK_FIFOSIZE = 11,
@@ -13,7 +17,7 @@ module noc_block_zero_pad #(
   output [63:0] debug
 );
 
-  ////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
   //
   // RFNoC Shell
   //
@@ -21,8 +25,6 @@ module noc_block_zero_pad #(
   wire [31:0] set_data;
   wire [7:0]  set_addr;
   wire        set_stb;
-  reg  [63:0] rb_data;
-  wire [7:0]  rb_addr;
 
   wire [63:0] cmdout_tdata, ackin_tdata;
   wire        cmdout_tlast, cmdout_tvalid, cmdout_tready, ackin_tlast, ackin_tvalid, ackin_tready;
@@ -30,11 +32,8 @@ module noc_block_zero_pad #(
   wire [63:0] str_sink_tdata, str_src_tdata;
   wire        str_sink_tlast, str_sink_tvalid, str_sink_tready, str_src_tlast, str_src_tvalid, str_src_tready;
 
-  wire [15:0] src_sid;
-  wire [15:0] next_dst_sid, resp_out_dst_sid;
-  wire [15:0] resp_in_dst_sid;
-
   wire        clear_tx_seqnum;
+  wire [15:0] src_sid, next_dst_sid;
 
   noc_shell #(
     .NOC_ID(NOC_ID),
@@ -46,8 +45,8 @@ module noc_block_zero_pad #(
     // Computer Engine Clock Domain
     .clk(ce_clk), .reset(ce_rst),
     // Control Sink
-    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb),
-    .rb_stb(1'b1), .rb_data(rb_data), .rb_addr(rb_addr),
+    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb), .set_time(),
+    .rb_stb(1'b1), .rb_data(64'd0), .rb_addr(),
     // Control Source
     .cmdout_tdata(cmdout_tdata), .cmdout_tlast(cmdout_tlast), .cmdout_tvalid(cmdout_tvalid), .cmdout_tready(cmdout_tready),
     .ackin_tdata(ackin_tdata), .ackin_tlast(ackin_tlast), .ackin_tvalid(ackin_tvalid), .ackin_tready(ackin_tready),
@@ -55,13 +54,7 @@ module noc_block_zero_pad #(
     .str_sink_tdata(str_sink_tdata), .str_sink_tlast(str_sink_tlast), .str_sink_tvalid(str_sink_tvalid), .str_sink_tready(str_sink_tready),
     // Stream Source
     .str_src_tdata(str_src_tdata), .str_src_tlast(str_src_tlast), .str_src_tvalid(str_src_tvalid), .str_src_tready(str_src_tready),
-    // Stream IDs set by host 
-    .src_sid(src_sid),                   // SID of this block
-    .next_dst_sid(next_dst_sid),         // Next destination SID
-    .resp_in_dst_sid(resp_in_dst_sid),   // Response destination SID for input stream responses / errors
-    .resp_out_dst_sid(resp_out_dst_sid), // Response destination SID for output stream responses / errors
-    // Misc
-    .vita_time('d0), .clear_tx_seqnum(clear_tx_seqnum),
+    .clear_tx_seqnum(clear_tx_seqnum), .src_sid(src_sid), .next_dst_sid(next_dst_sid), .resp_in_dst_sid(), .resp_out_dst_sid(),
     .debug(debug));
 
   ////////////////////////////////////////////////////////////
@@ -70,25 +63,25 @@ module noc_block_zero_pad #(
   // Convert RFNoC Shell interface into AXI stream interface
   //
   ////////////////////////////////////////////////////////////
-  wire [WIDTH-1:0] m_axis_data_tdata;
-  wire        m_axis_data_tlast;
-  wire        m_axis_data_tvalid;
-  wire        m_axis_data_tready;
-
-  wire [WIDTH-1:0] s_axis_data_tdata;
-  wire        s_axis_data_tlast;
-  wire        s_axis_data_tvalid;
-  wire        s_axis_data_tready;
-  wire [127:0] s_axis_data_tuser;
+  wire [31:0]  m_axis_data_tdata;
+  wire         m_axis_data_tlast;
+  wire         m_axis_data_tvalid;
+  wire         m_axis_data_tready;
   wire [127:0] m_axis_data_tuser;
 
+  wire [31:0]  s_axis_data_tdata;
+  wire         s_axis_data_tlast;
+  wire         s_axis_data_tvalid;
+  wire         s_axis_data_tready;
+  wire [127:0] s_axis_data_tuser;
+
   axi_wrapper #(
-    .SIMPLE_MODE(1))
-  axi_wrapper (
+    .SIMPLE_MODE(0))
+  inst_axi_wrapper (
     .clk(ce_clk), .reset(ce_rst),
     .clear_tx_seqnum(clear_tx_seqnum),
     .next_dst(next_dst_sid),
-    .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
+    .set_stb(), .set_addr(), .set_data(),
     .i_tdata(str_sink_tdata), .i_tlast(str_sink_tlast), .i_tvalid(str_sink_tvalid), .i_tready(str_sink_tready),
     .o_tdata(str_src_tdata), .o_tlast(str_src_tlast), .o_tvalid(str_src_tvalid), .o_tready(str_src_tready),
     .m_axis_data_tdata(m_axis_data_tdata),
@@ -114,9 +107,6 @@ module noc_block_zero_pad #(
   // User code
   //
   ////////////////////////////////////////////////////////////
-  // NoC Shell registers 0 - 127,
-  // User register address space starts at 128
-  localparam SR_USER_REG_BASE = 128;
 
   // Control Source Unused
   assign cmdout_tdata  = 64'd0;
@@ -124,57 +114,24 @@ module noc_block_zero_pad #(
   assign cmdout_tvalid = 1'b0;
   assign ackin_tready  = 1'b1;
 
-  // Settings registers
-  //
-  // - The settings register bus is a simple strobed interface.
-  // - Transactions include both a write and a readback.
-  // - The write occurs when set_stb is asserted.
-  //   The settings register with the address matching set_addr will
-  //   be loaded with the data on set_data.
-  // - Readback occurs when rb_stb is asserted. The read back strobe
-  //   must assert at least one clock cycle after set_stb asserts /
-  //   rb_stb is ignored if asserted on the same clock cycle of set_stb.
-  //   Example valid and invalid timing:
-  //              __    __    __    __
-  //   clk     __|  |__|  |__|  |__|  |__
-  //               _____
-  //   set_stb ___|     |________________
-  //                     _____
-  //   rb_stb  _________|     |__________     (Valid)
-  //                           _____
-  //   rb_stb  _______________|     |____     (Valid)
-  //           __________________________
-  //   rb_stb                                 (Valid if readback data is a constant)
-  //               _____
-  //   rb_stb  ___|     |________________     (Invalid / ignored, same cycle as set_stb)
-  //
-  localparam [7:0] SR_TEST_REG_0 = SR_USER_REG_BASE;
-  localparam [7:0] SR_TEST_REG_1 = SR_USER_REG_BASE + 8'd1;
+  localparam [7:0] SR_N           = 129;
+  localparam [7:0] SR_VECTOR_MODE = 130;
 
-  wire [31:0] test_reg_0;
+  wire [15:0] n;
   setting_reg #(
-    .my_addr(SR_TEST_REG_0), .awidth(8), .width(32))
-  sr_test_reg_0 (
+    .my_addr(SR_N), .awidth(8), .width(16), .at_reset(1))
+  sr_n (
     .clk(ce_clk), .rst(ce_rst),
-    .strobe(set_stb), .addr(set_addr), .in(set_data), .out(test_reg_0), .changed());
+    .strobe(set_stb), .addr(set_addr), .in(set_data), .out(n), .changed());
 
-  wire [31:0] test_reg_1;
+  wire vector_mode;
   setting_reg #(
-    .my_addr(SR_TEST_REG_1), .awidth(8), .width(32))
-  sr_test_reg_1 (
+    .my_addr(SR_VECTOR_MODE), .awidth(8), .width(1), .at_reset(1) /* Drop vectors by default */)
+  sr_vector_mode (
     .clk(ce_clk), .rst(ce_rst),
-    .strobe(set_stb), .addr(set_addr), .in(set_data), .out(test_reg_1), .changed());
+    .strobe(set_stb), .addr(set_addr), .in(set_data), .out(vector_mode), .changed());
 
-  // Readback registers
-  // rb_stb set to 1'b1 on NoC Shell
-  always @(posedge ce_clk) begin
-    case(rb_addr)
-      8'd0 : rb_data <= {32'd0, test_reg_0};
-      8'd1 : rb_data <= {32'd0, test_reg_1};
-      default : rb_data <= 64'h0BADC0DE0BADC0DE;
-    endcase
-  end
-
+  // Modify packet header data, same as input except SRC / DST SID fields.
   cvita_hdr_modify cvita_hdr_modify (
     .header_in(m_axis_data_tuser),
     .header_out(s_axis_data_tuser),
@@ -186,7 +143,11 @@ module noc_block_zero_pad #(
     .use_src_sid(1'b1),   .src_sid(src_sid),
     .use_dst_sid(1'b1),   .dst_sid(next_dst_sid),
     .use_vita_time(1'b0), .vita_time());
-  
+
+  // Note: When dropping samples (i.e. vector_mode = 0), unless this block receives a multiple
+  //       of N packets (where N is the decimation rate), there will be a partial formed output
+  //       packet. This partial formed packet must be cleared before restarting the block. We
+  //       can use clear_tx_seqnum for that purpose as it is strobed at block initialization.
   zero_pad #(
 	  .WIDTH(WIDTH),
 	  .OUT_L(OUT_L)
